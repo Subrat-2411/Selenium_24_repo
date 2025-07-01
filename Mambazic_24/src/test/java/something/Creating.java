57a34cde4412890b9aec60060c34d79c49052f27
Rutusmita prusty <Rutusmita prusty@LAPTOP-K6JBIRD9>
2025-07-01 13:15:10 +0530

    Added Creating

---
+++ Mambazic_24/src/test/java/something/Creating.java
@@ -0,0 +1,18 @@
+package something;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Creating {
+	@Test
+	public void demo()
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get("https://www.flipkart.com/");
+	}
+}
